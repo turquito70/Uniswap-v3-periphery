b73f5f7e6918160f37c800053275ae66ec22454b
turquito70 <113914568+turquito70@users.noreply.github.com>
2024-07-17 22:10:10 -0400

    PoolAddress.sol

--- contracts/libraries/PoolAddress.sol
+++ contracts/libraries/PoolAddress.sol
@@ -38,9 +38,8 @@ library PoolAddress {
     * Change to:
     */
 
-    function computeAddress(address factory, PoolKey memory key) internal pure returns (address poolHash) {
+    function computeAddress(address factory, PoolKey memory key) internal pure returns (address pool) {
         require(key.token0 < key.token1);
-        address pool; // Declare here
-        pool = address(uint160(uint256(keccak256(abi.encodePacked(hex'ff', factory, keccak256(abi.encode(key.token0, key.token1, key.fee)), POOL_INIT_CODE_HASH)))));
+        return address(uint160(uint256(keccak256(abi.encodePacked(hex'ff', factory, keccak256(abi.encode(key.token0, key.token1, key.fee)), POOL_INIT_CODE_HASH)))));
     }
 }
