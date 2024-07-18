44059347267beca94fd88a805068656d0fa2b33c
turquito70 <113914568+turquito70@users.noreply.github.com>
2024-07-17 20:22:27 -0400

    PoolAddress.sol

--- contracts/libraries/PoolAddress.sol
+++ contracts/libraries/PoolAddress.sol
@@ -30,19 +30,17 @@ library PoolAddress {
     /// @param factory The Uniswap V3 factory contract address
     /// @param key The PoolKey
     /// @return pool The contract address of the V3 pool
-    function computeAddress(address factory, PoolKey memory key) internal pure returns (address pool) {
+    /**
+    * function computeAddress(address factory, PoolKey memory key) internal pure returns (address pool) {
+    *    require(key.token0 < key.token1);
+    *    pool = address(uint256(keccak256(abi.encodePacked(hex'ff',factory,keccak256(abi.encode(key.token0, key.token1, key.fee)),POOL_INIT_CODE_HASH))));
+    * }
+    * Change to:
+    */
+
+    function computeAddress(address factory, PoolKey memory key) internal pure returns (address poolHash) {
         require(key.token0 < key.token1);
-        pool = address(
-            uint256(
-                keccak256(
-                    abi.encodePacked(
-                        hex'ff',
-                        factory,
-                        keccak256(abi.encode(key.token0, key.token1, key.fee)),
-                        POOL_INIT_CODE_HASH
-                    )
-                )
-            )
-        );
+        address pool; // Declare here
+        pool = address(uint160(uint256(keccak256(abi.encodePacked(hex'ff', factory, keccak256(abi.encode(key.token0, key.token1, key.fee)), POOL_INIT_CODE_HASH)))));
     }
 }
